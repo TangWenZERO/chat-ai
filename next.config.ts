3c40f20748665f99fc2d7cf4499b789edde434b0
ext_tangwen <18300613164@163.com>
2025-08-24 03:08:04 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone", // 使得 Next.js 可在无 Node.js 服务器的环境下运行
+  experimental: {
+    outputStandalone: true,
+  },
 };
 
 export default nextConfig;
